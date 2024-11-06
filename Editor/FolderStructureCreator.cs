68364f06886bd7f3f219c3bbfab29cb1106a5e99
unknown <gabrielctorres38@gmail.com>
2024-11-06 10:30:23 -0300

    adicionado ferramenta para criacao de pastas

---
+++ Editor/FolderStructureCreator.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class FolderStructureCreator : EditorWindow
+{
+    [MenuItem("Tools/Create Default Project Folders")]
+    public static void CreateProjectFolders()
+    {
+
+        string rootFolder = "Assets/_Project";
+
+
+        string[] folders = {
+            $"{rootFolder}/Resources",
+            $"{rootFolder}/Resources/Prefab",
+            $"{rootFolder}/Resources/Art",
+            $"{rootFolder}/Resources/Sound",
+            $"{rootFolder}/Scenes",
+            $"{rootFolder}/Code",
+            $"{rootFolder}/Code/Scripts",
+            $"{rootFolder}/Code/Shader",
+            $"{rootFolder}/Settings"
+        };
+
+
+        foreach (string folder in folders)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+                Debug.Log($"Pasta criada: {folder}");
+            }
+            else
+            {
+                Debug.Log($"Pasta já existe: {folder}");
+            }
+        }
+
+
+        AssetDatabase.Refresh();
+        Debug.Log("Estrutura de pastas criada ou atualizada com sucesso!");
+    }
+}
